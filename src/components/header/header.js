cdf68562542a07a82f9b33fbd1dfff15639d1305
Dima <applestock@MacBook-Pro-Apple.local>
2020-05-31 22:01:19 +0300

    header animations

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -26,11 +26,28 @@ const MainNavigation = () => {
   )
 }
 
-const Header = () => {
+const Hamburger = () => {
   return (
-    <header className={headerStyles.mainHeader}>
+    <button className={headerStyles.hamburger}>
+      <svg height="20" viewBox="0 -53 384 384" width="20">
+        <path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/>
+        <path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/>
+        <path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/>
+      </svg>
+      <span>Main menu</span>
+    </button>
+  )
+}
+
+
+
+const Header = (props) => {
+  return (
+
+    <header id={props} className={headerStyles.mainHeader}>
       <MainLogo />
       <MainNavigation />
+      <Hamburger />
     </header>
   );
 }
