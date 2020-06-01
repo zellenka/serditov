134296e4ca7cee99a4b4f8dc3479f05b77ea19a7
Serhii <seleshhenko@yahoo.com>
2020-06-01 10:09:01 +0300

    save commit

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -18,19 +18,20 @@ class CarouselSlider extends React.Component {
   render() {
     return (
       <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={100}
+        naturalSlideWidth={140}
+        naturalSlideHeight={120}
         totalSlides={5}
         visibleSlides={3}
         step={1}
       >
         <div className="dots">
-          <Dot disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/the_residence.jpg" />} slide={0} />
-          <Dot disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_20.jpg" />} slide={1} />
-          <Dot disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_01-375x250.jpg" />} slide={2} />
-          <Dot disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_12.jpg" />} slide={3} />
-          <Dot disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_knights_wood_10-RT-375x250.jpg" />} slide={4} />
+          <Dot className={'thumbnail__button'} disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/the_residence.jpg" />} slide={0} />
+          <Dot className={'thumbnail__button'} disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_20.jpg" />} slide={1} />
+          <Dot className={'thumbnail__button'} disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_01-375x250.jpg" />} slide={2} />
+          <Dot className={'thumbnail__button'} disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_12.jpg" />} slide={3} />
+          <Dot className={'thumbnail__button'} disabled={false} children={<Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_knights_wood_10-RT-375x250.jpg" />} slide={4} />
         </div>
+        
         <Slider>
           <Slide index={0}><Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/the_residence.jpg" /></Slide>
           <Slide index={1}><Image src="http://www.honky.co.uk/site/wp-content/uploads/2015/04/ab_frognal_way_20.jpg" /></Slide>
@@ -51,7 +52,6 @@ const Showcase = ({location}) => {
     <Layout location={location}>
       <Seo />
       <CarouselSlider />
-      <p>Such wow. Very React.</p>
       </Layout>
   )
 }
