acc03e0a72702911da956ea5faed15c43ef09ff1
Serhii <seleshhenko@yahoo.com>
2020-06-12 22:33:53 +0300

    save commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,20 +6,17 @@ const Layout = (props) => {
   let pageClass = '';
   props.location.pathname === '/' ? pageClass = '-main' : pageClass = props.location.pathname.replace('/', '-');
   
-  const [thumbnails, toggleThumbnails] = React.useState(props.thumb);
+  const [thumbnails, toggleThumbnails] = React.useState(false);
 
   const thumbnailsToggler = () => toggleThumbnails(!thumbnails);
 
-  console.log(thumbnails);
-
-  useEffect(() => {
-    props.thumb ? toggleThumbnails(false)
-  });
+  const myOwnProperties = { foo: thumbnails };
+  // console.log(thumbnails);
   
   return (
     <div className={`page${pageClass} ${thumbnails  ? 'thumbnails_opened' : ``}`}>
       <Header clickHandler={thumbnailsToggler}/>
-      {props.children }
+      {props.children({ ...props, ...myOwnProperties })}
     </div>
   )
 }
